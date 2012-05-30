79d7c1fe201c6371f6ec9815be481ae9c3ffaae0
Nato Saichek <natosaichek@gmail.com>
2012-05-30 09:30:28 -0700

    got generations to elapse in the crappy version of the GA system

--- Assets/Scripts/natoworld.js
+++ Assets/Scripts/natoworld.js
@@ -122,25 +122,57 @@ class CartGenome extends GaGenome{
 /*  A cartmaker is an assembly routine that knows how to put together a cart given a cartgenome.
 	Eventually this will probably go away as a more generic assember is written  - one that can
 	validate a genome and assemble the object it codes for. */
+class Cart {
+	
+	var components:GameObject[];
+	function Cart(numComponents) {
+		this.components = new GameObject[numComponents];
+	}
+	function destroy() {
+		for(var i = 0; i< this.components.Length; i++) {
+			GameObject.Destroy(this.components[i]);
+		}
+	}
+	}
 
 class CartMaker {
 	var numcarts:int;
 	var gaController:natoworld;
-	var carts:GameObject[];
+	var carts:Cart[];
+	var cartStartPos:Vector3[];
+	var genomes:CartGenome[];
 	
 	function CartMaker(numcarts:int,caller:natoworld) { 
 		this.numcarts = numcarts;
 		this.gaController = caller;
-		this.carts = new GameObject[numcarts];
+		this.carts = new Cart[numcarts];
+		this.cartStartPos = new Vector3[numcarts];
+		this.genomes = new CartGenome[numcarts];
 	}
 	function makeCarts() {
 		for(var i = 0; i<this.numcarts; i++) {
 			var name:String;
 			var position:Vector3;
 			name = "cartnumber"+i.ToString;
-			position = Vector3(i*4 - 10,5,0);
+			position = Vector3(i*5,5,0);
 			var genome = new CartGenome();
 			this.carts[i] = (this.makeCart(i,name,position,genome));
+			Debug.Log("Instantiated cart"+i+" at position "+position);			
+			this.cartStartPos[i] = position;
+			this.genomes[i] = genome;
+		}
+		return this.carts;
+	}	
+	function makeCarts(genomes:CartGenome[]) {
+		for(var i = 0; i<genomes.Length; i++) {
+			var name:String;
+			var position:Vector3;
+			name = "cartnumber"+i.ToString;
+			position = Vector3(i*5,5,0);
+			var genome = genomes[i];
+			this.carts[i] = (this.makeCart(i,name,position,genome));
+			this.cartStartPos[i] = position;
+			this.genomes[i] = genome;
 		}
 		return this.carts;
 	}	
@@ -155,7 +187,7 @@ class CartMaker {
 		var drivetotal:float = genome.motorAllocation+genome.powerPackAllocation;
 		var motormass = (genome.motorAllocation/drivetotal)*genome.drivepowerMass;
 		var powerpackmass = (genome.powerPackAllocation/drivetotal)*genome.drivepowerMass;
-		var cart:GameObject = this.gaController.loadObject("cartBase",position,false);
+		var cartbase:GameObject = this.gaController.loadObject("cartBase",position,false);
 		
 		// If our motor or power pack are too light, then the joints will make them go crazy.  Don't let that happen:
 		if (motormass>(basemass/10) && motormass<(basemass*10)){
@@ -171,18 +203,19 @@ class CartMaker {
 		
 		// It's possible at this point that one of our components may be nonexistent.
 		if (motor == null || powerpack == null){
-			Debug.Log("unable to load some object - motor or powerpack.  Returning a null cart.");
-			return null;
+			Debug.Log("unable to load some object - motor or powerpack.  Returning an empty cart.");
+			return Cart(0);
 		}else{
 			// Our component masses are fine.  Lets create them.
-			cart.rigidbody.mass = basemass;
+			cartbase.rigidbody.mass = basemass;
 			motor.rigidbody.mass = motormass;
 			powerpack.rigidbody.mass = powerpackmass;
+			var thiscart = Cart(genome.numWheels+3);
 			
 			// connect the joint to the motor.  It will start at the motor, go to the cart.  It has a center at the center of the motor(0,0,0), and a primary axis of (1,0,0) - the x axis.
-			var motorjoint = gaController.j.createJoint(motor,cart,Vector3(0,0,0),Vector3(1,0,0));
+			var motorjoint = gaController.j.createJoint(motor,cartbase,Vector3(0,0,0),Vector3(1,0,0));
 			// connect the joint to the powerpack.  It will start at the pack, go to the cart.  It has a center at the center of the pack(0,0,0), and a primary axis of (1,0,0) - the x axis.
-			var packjoint = gaController.j.createJoint(powerpack,cart,Vector3(0,0,0),Vector3(1,0,0));
+			var packjoint = gaController.j.createJoint(powerpack,cartbase,Vector3(0,0,0),Vector3(1,0,0));
 			
 			Debug.Log("build wheels");
 			//var wheels = Array(genome.numWheels);
@@ -191,12 +224,21 @@ class CartMaker {
 				wg = genome.WheelGenomes[i];
 				var wheel:GameObject = this.gaController.loadObject(wg.wheelType,position + genome.wheelPositions[i]*structuralDistance,false);
 				wheel.transform.Rotate(wg.initialAngle);
-				var joint:ConfigurableJoint = gaController.j.createJoint(wheel,cart,Vector3(0,0,0),Vector3(1,0,0));
+				var joint:ConfigurableJoint = gaController.j.createJoint(wheel,cartbase,Vector3(0,0,0),Vector3(1,0,0));
 				joint.angularYMotion=ConfigurableJointMotion.Free;
-				joint.targetAngularVelocity=Vector3(0,1,0);
-				joint.angularYZDrive.mode=JointDriveMode.Velocity;	
+				joint.targetAngularVelocity=Vector3(0,5,0);
+				joint.angularYZDrive.mode=JointDriveMode.Velocity;
+				thiscart.components[i] = wheel;
+				Debug.Log("wheel "+i);	
 			}
-			return cart;
+			Debug.Log("i = "+i+" and we've got room for "+ thiscart.components.Length + "elements");
+			thiscart.components[i] = cartbase;
+			Debug.Log("i = "+(i+1)+" and we've got room for "+ thiscart.components.Length + "elements");
+			thiscart.components[i+1] = motor;
+			Debug.Log("i = "+(i+2)+" and we've got room for "+ thiscart.components.Length + "elements");
+			thiscart.components[i+2] = powerpack;
+			
+			return thiscart;
 		}
 	}
 }
@@ -210,13 +252,33 @@ class CartMaker {
 	mechanism. */
 
 class CartEvaluator {
-	function CartEvaluator() {
+	var maker:CartMaker;
+	function CartEvaluator(maker:CartMaker) {
+		this.maker = maker;		
 	}
-	function evaluate(cart:GameObject,duration:int) {
-		checkForCollisions(cart);
+	function evaluate(cart:Cart, index:int) {
+		Debug.Log("Evaluating cart");
+		// We'll evaluate based on distance traveled from start position.
+		if (cart.components.Length > 3) {
+		var cartbase = cart.components[cart.components.Length-3]; 
+		var endpos:Vector3 = cartbase.rigidbody.transform.position;
+		var startpos:Vector3 = this.maker.cartStartPos[index];
+		var dist:Vector3 = (endpos-startpos);
+		var result:float = dist.magnitude;
+		return result;
+		}
+		else
+		return 0;
 	}
-	function checkForCollisions(cart:GameObject) {
+	function evaluateCarts() {
+		var metrics:float[] = new float[this.maker.carts.Length];
+		Debug.Log("maker has "+maker.carts.Length+" carts");
+		for(var i = 0; i<this.maker.carts.Length; i++) {
+			metrics[i] = this.evaluate(this.maker.carts[i],i);
+		}
+		return metrics;
 	}
+
 }
 
 /*  Once carts (or other generic mechanisms) have been evaluated, they must reproduce.
@@ -227,19 +289,59 @@ class CartEvaluator {
 class CartReplicator {
 	function CartReplicator() {
 	}
+	function replicate(genomes:CartGenome[],metrics:float[]) {
+		return genomes;
+	}
 }
 
 
 /*  Here we set up the initial conditions and build each of the components of the GA
 	process.*/
+var numGenerations:int = 5;
+var genTime = 10; //10 seconds per generation
+var numcarts = 5;
+var debugTime:float = 1;
+var replicator:CartReplicator = new CartReplicator();
+var maker:CartMaker = new CartMaker(numcarts,this);
+var evaluator:CartEvaluator = new CartEvaluator(maker);
+var metrics:float[] = new float[numcarts];
+var cartGenomes:CartGenome[] = new CartGenome[numcarts];
+var newGenomes:CartGenome[] = new CartGenome[numcarts];
+var carts:Cart[] = new Cart[numcarts];
+var generationStartTime:float;
+var debugStartTime:float;
 
 function Start() {
-	var evaluator:CartEvaluator = new CartEvaluator();
-	var replicator:CartReplicator = new CartReplicator();
-	var maker:CartMaker = new CartMaker(5,this);
-	maker.makeCarts();	
+	carts = maker.makeCarts();
+	cartGenomes = maker.genomes;
+	generationStartTime = Time.time;
+	debugStartTime = Time.time;
+	
 }
-
+function Update () {
+	for(var i = 0; i<numGenerations; i++) {
+		if(Time.time > debugStartTime+debugTime) {
+			Debug.Log("One Second");
+			debugStartTime = Time.time;
+		}
+		if(Time.time > generationStartTime+genTime)
+		{
+			generationStartTime = Time.time - .1;
+			Debug.Log("One Generation");
+			// wait for gentime to pass, then evaluate, kill, and reproduce.
+			metrics = evaluator.evaluateCarts();
+			for (var j = 0; j<maker.carts.Length; j++) {
+				maker.carts[j].destroy();
+				}
+			Debug.Log("Metrics:" + metrics);
+			Debug.Log("For Genomes:" +maker.genomes);
+			
+			newGenomes = replicator.replicate(maker.genomes,metrics);		
+			carts = maker.makeCarts(newGenomes);
+			generationStartTime = Time.time;
+		}
+	}
+}
 
 /* Helper functions */
 function loadObject(objType:String,objpos:Vector3,addscript:boolean)
